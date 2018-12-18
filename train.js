0687460b953202fbc7da3a55d305cd619babade3
kmop <42947972+AKluger@users.noreply.github.com>
2018-12-18 11:16:39 -0500

    Erased superfluous code, added table border and darkened header, added clearing of form jquery.

--- train.js
+++ train.js
@@ -1,6 +1,4 @@
-
 // Initialize Firebase
-
 var config = {
     apiKey: "AIzaSyAGubFMxHrOGpJVfIoWVMVf6ERDKx5zg8Y",
     authDomain: "train-scheduler-91ad1.firebaseapp.com",
@@ -17,7 +15,6 @@ var database = firebase.database();
 
 // Capture Button Click
 $("#add-train").on("click", function (event) {
-    // Don't refresh the page!
     event.preventDefault();
 
     var trainName = $("#name-input").val().trim()
@@ -25,7 +22,6 @@ $("#add-train").on("click", function (event) {
     var firstTime = $("#time-input").val().trim()
     var destination = $("#destination-input").val().trim()
 
- 
     // Code for handling the push
     database.ref().push({
         name: trainName,
@@ -34,51 +30,38 @@ $("#add-train").on("click", function (event) {
         destination: destination,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-
+    //clear form
+    $("input").val("")
 });
 
 database.ref().on("child_added", function (snapshot) {
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
-
-
-    //.text replaces the text on a  page
     // Console.logging the last user's data
     console.log(sv.name);
     console.log(sv.frequency);
     console.log(sv.firstTime);
     console.log(sv.destination);
-    // console.log(sv.dateAdded);
-
-
-
-// somehow think I want diff in firstTime and currentTime, then I could decide if one is greater than the other use this value 
-// Using remainder seems like the way to go, 
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(sv.firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
+  
     // Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
     // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "days");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
     // Time apart (remainder)
     var tRemainder = diffTime % sv.frequency;
-    console.log(tRemainder);
 
-    // Minute Until Train
-    var tMinutesTillTrain = sv.frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Minutes Until Train
+    var minutesAway = sv.frequency - tRemainder;
 
     // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrain = moment().add(minutesAway, "minutes");
     nextTrain = moment(nextTrain).format("HH:mm")
-    console.log("ARRIVAL TIME: " + nextTrain);
 
     var subtracted = moment().diff(sv.firstTime, 'minutes');
 
@@ -87,10 +70,8 @@ database.ref().on("child_added", function (snapshot) {
         "<td> " + sv.destination + " </td>" +
         "<td> " + sv.frequency + " </td>" +
         "<td> " + nextTrain + " </td>" +
-        "<td> " + tMinutesTillTrain + " </td>"
+        "<td> " + minutesAway + " </td>"
     )
-
-
 
     // Handle the errors
 }, function (errorObject) {
