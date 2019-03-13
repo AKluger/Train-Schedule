2be976f3438dc7335aae8a31d3df16a952615083
kmop <42947972+AKluger@users.noreply.github.com>
2019-03-13 13:37:46 -0400

    updated var to const and added favicon

--- train.js
+++ train.js
@@ -1,7 +1,5 @@
-
-var update;
 // Initialize Firebase
-var config = {
+const config = {
     apiKey: "AIzaSyAGubFMxHrOGpJVfIoWVMVf6ERDKx5zg8Y",
     authDomain: "train-scheduler-91ad1.firebaseapp.com",
     databaseURL: "https://train-scheduler-91ad1.firebaseio.com",
@@ -13,16 +11,16 @@ var config = {
 firebase.initializeApp(config);
 
 // Create a variable to reference the database.
-var database = firebase.database();
+const database = firebase.database();
 
 // Capture Button Click
 $("#add-train").on("click", function (event) {
     event.preventDefault();
 
-    var trainName = $("#name-input").val().trim()
-    var frequency = $("#frequency-input").val().trim()
-    var firstTime = $("#time-input").val().trim()
-    var destination = $("#destination-input").val().trim()
+    const trainName = $("#name-input").val().trim()
+    const frequency = $("#frequency-input").val().trim()
+    const firstTime = $("#time-input").val().trim()
+    const destination = $("#destination-input").val().trim()
 
     // Code for handling the push
     database.ref().push({
@@ -39,41 +37,32 @@ $("#add-train").on("click", function (event) {
     updater()
 });
 
-var updater = function () {
+const updater = function () {
 
     //empty table so only updated times display
     $("tbody").empty()
 
     database.ref().on("child_added", function (snapshot) {
         // storing the snapshot.val() in a variable for convenience
-        var sv = snapshot.val();
-        // Console.logging the last user's data
-        console.log(sv.name);
-        console.log(sv.frequency);
-        console.log(sv.firstTime);
-        console.log(sv.destination);
+        const sv = snapshot.val();
 
         // First Time (pushed back 1 year to make sure it comes before current time)
-        var firstTimeConverted = moment(sv.firstTime, "HH:mm").subtract(1, "years");
-
-        // Current Time
-        var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+        const firstTimeConverted = moment(sv.firstTime, "HH:mm").subtract(1, "years");
 
         // Difference between the times
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        const diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
         // Time apart (remainder)
-        var tRemainder = diffTime % sv.frequency;
+        const tRemainder = diffTime % sv.frequency;
 
         // Minutes Until Train
-        var minutesAway = sv.frequency - tRemainder;
+        const minutesAway = sv.frequency - tRemainder;
 
         // Next Train
-        var nextTrain = moment().add(minutesAway, "minutes");
+        let nextTrain = moment().add(minutesAway, "minutes");
         nextTrain = moment(nextTrain).format("HH:mm")
 
-        var subtracted = moment().diff(sv.firstTime, 'minutes');
+        const subtracted = moment().diff(sv.firstTime, 'minutes');
 
         // Change the HTML to reflect
         $("tbody").append("<tr>  <td > " + sv.name + " </td>" +
@@ -89,14 +78,13 @@ var updater = function () {
     });
 
     //keeping site up to date to the second
-    currentTime = moment().format("HH:mm");
-    var checkTime = function () {
+   const currentTime = moment().format("HH:mm");
+    const checkTime = function () {
         update = setTimeout(checkTime, 1000)
         if (currentTime !== moment().format("HH:mm")) {
             updater()
         }
     }
-
     //countdown function to refresh page when time changes
     checkTime()
 }
